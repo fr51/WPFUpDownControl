a07c00026d4254ff3b5a91e31b3048dc363e260b
fr51 <francoisemile5@orange.fr>
2024-10-11 23:02:12 +0200

    adding base markup

---
+++ WPFUpDownControl/UpDownControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace WPFUpDownControl
+{
+	/// <summary>
+	/// interaction logic for UpDownControl.xaml
+	/// </summary>
+	public partial class UpDownControl : UserControl
+	{
+		/// <summary>
+		/// constructor
+		/// </summary>
+		public UpDownControl ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
