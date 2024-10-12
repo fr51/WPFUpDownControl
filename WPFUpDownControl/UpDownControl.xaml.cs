6b1e2af4c2e9ab531719cdbaaf876968ccbc73b3
fr51 <francoisemile5@orange.fr>
2024-10-12 10:57:34 +0200

    adding current value

--- WPFUpDownControl/UpDownControl.xaml.cs
+++ WPFUpDownControl/UpDownControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace WPFUpDownControl
 {
@@ -8,11 +11,113 @@ namespace WPFUpDownControl
 	public partial class UpDownControl : UserControl
 	{
 		/// <summary>
+		/// Exposes the <see cref="CurrentValue"/> property
+		/// </summary>
+		public static readonly DependencyProperty CurrentValueProperty=DependencyProperty.Register ("CurrentValue", typeof (decimal?), typeof (UpDownControl), new PropertyMetadata (null, OnCurrentValuePropertyChanged));
+
+		/// <summary>
+		/// Exposes the <see cref="CurrentValueChanged"/> event
+		/// </summary>
+		public static readonly RoutedEvent CurrentValueChangedEvent=EventManager.RegisterRoutedEvent ("CurrentValueChanged", RoutingStrategy.Bubble, typeof (RoutedEventHandler), typeof (UpDownControl));
+
+		/// <summary>
+		/// This event triggers when the <see cref="CurrentValue"/> changes
+		/// </summary>
+		[Description ("Triggered when the current value changes")]
+		public event RoutedEventHandler CurrentValueChanged
+		{
+			add
+			{
+				AddHandler (CurrentValueChangedEvent, value);
+			}
+			remove
+			{
+				RemoveHandler (CurrentValueChangedEvent, value);
+			}
+		}
+
+		/// <summary>
+		/// This is the counter's value
+		/// </summary>
+		[Bindable (true)]
+		public decimal? CurrentValue
+		{
+			get
+			{
+				return ((decimal?) GetValue (CurrentValueProperty));
+			}
+			set
+			{
+				this.CheckCurrentValue (value);
+				SetValue (CurrentValueProperty, value);
+			}
+		}
+
+		/// <summary>
 		/// constructor
 		/// </summary>
 		public UpDownControl ()
 		{
 			InitializeComponent ();
+
+			this.Loaded+=this.UpDownControl_Loaded;
+		}
+
+		/// <summary>
+		/// callback triggered when the <see cref="CurrentValueProperty"/> changes
+		/// </summary>
+		/// <param name="dependencyObject">
+		/// the <see cref="DependencyObject"/> where the property changed
+		/// </param>
+		/// <param name="dependencyPropertyChangedEventArgs">
+		/// some change-related data
+		/// </param>
+		private static void OnCurrentValuePropertyChanged (DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+		{
+			UpDownControl upDownControl=(UpDownControl) dependencyObject;
+
+			upDownControl.OnCurrentValueChanged ();
+		}
+
+		/// <summary>
+		/// raises the <see cref="CurrentValueChangedEvent"/> event
+		/// </summary>
+		protected void OnCurrentValueChanged ()
+		{
+			RoutedEventArgs routedEventArgs=new RoutedEventArgs (CurrentValueChangedEvent);
+
+			RaiseEvent (routedEventArgs);
+		}
+
+		/// <summary>
+		/// checks <see cref="CurrentValue"/> fulfills some constraints
+		/// </summary>
+		/// <param name="value">
+		/// the <see cref="CurrentValue"/>
+		/// </param>
+		/// <exception cref="Exception">
+		/// thrown when one of the constraints isn't fulfilled
+		/// </exception>
+		private void CheckCurrentValue (decimal? value)
+		{
+			if (value==null)
+			{
+				throw new Exception ("Current value must be defined");
+			}
+		}
+
+		/// <summary>
+		/// performs some checks and adjustments when the control is ready to use
+		/// </summary>
+		/// <param name="sender">
+		/// the <see cref="UpDownControl"/> control
+		/// </param>
+		/// <param name="routedEventArgs">
+		/// some event-related data
+		/// </param>
+		private void UpDownControl_Loaded (object sender, RoutedEventArgs routedEventArgs)
+		{
+			this.CheckCurrentValue (this.CurrentValue);
 		}
 	}
 }
