561e5de737c9e37f4105b623beac2f58bba944d0
fr51 <francoisemile5@orange.fr>
2024-10-12 11:11:34 +0200

    adding step value

--- WPFUpDownControl/UpDownControl.xaml.cs
+++ WPFUpDownControl/UpDownControl.xaml.cs
@@ -14,11 +14,19 @@ namespace WPFUpDownControl
 		/// Exposes the <see cref="CurrentValue"/> property
 		/// </summary>
 		public static readonly DependencyProperty CurrentValueProperty=DependencyProperty.Register ("CurrentValue", typeof (decimal?), typeof (UpDownControl), new PropertyMetadata (null, OnCurrentValuePropertyChanged));
+		/// <summary>
+		/// Exposes the <see cref="Step"/> property
+		/// </summary>
+		public static readonly DependencyProperty StepProperty=DependencyProperty.Register ("Step", typeof (decimal?), typeof (UpDownControl), new PropertyMetadata (null, OnStepPropertyChanged));
 
 		/// <summary>
 		/// Exposes the <see cref="CurrentValueChanged"/> event
 		/// </summary>
 		public static readonly RoutedEvent CurrentValueChangedEvent=EventManager.RegisterRoutedEvent ("CurrentValueChanged", RoutingStrategy.Bubble, typeof (RoutedEventHandler), typeof (UpDownControl));
+		/// <summary>
+		/// Exposes the <see cref="StepChanged"/> event
+		/// </summary>
+		public static readonly RoutedEvent StepChangedEvent=EventManager.RegisterRoutedEvent ("StepChanged", RoutingStrategy.Bubble, typeof (RoutedEventHandler), typeof (UpDownControl));
 
 		/// <summary>
 		/// This event triggers when the <see cref="CurrentValue"/> changes
@@ -33,6 +41,21 @@ namespace WPFUpDownControl
 			remove
 			{
 				RemoveHandler (CurrentValueChangedEvent, value);
+			}
+		}
+		/// <summary>
+		/// This event triggers when the <see cref="Step"/> changes
+		/// </summary>
+		[Description ("Triggered when the step value changes")]
+		public event RoutedEventHandler StepChanged
+		{
+			add
+			{
+				AddHandler (StepChangedEvent, value);
+			}
+			remove
+			{
+				RemoveHandler (StepChangedEvent, value);
 			}
 		}
 
@@ -50,6 +73,22 @@ namespace WPFUpDownControl
 			{
 				this.CheckCurrentValue (value);
 				SetValue (CurrentValueProperty, value);
+			}
+		}
+		/// <summary>
+		/// This is the value which will be added/subtracted when you increase/decrease the <see cref="CurrentValue"/>
+		/// </summary>
+		[Bindable (true)]
+		public decimal? Step
+		{
+			get
+			{
+				return ((decimal?) GetValue (StepProperty));
+			}
+			set
+			{
+				this.CheckStep (value);
+				SetValue (StepProperty, value);
 			}
 		}
 
@@ -90,6 +129,32 @@ namespace WPFUpDownControl
 		}
 
 		/// <summary>
+		/// callback triggered when the <see cref="StepProperty"/> changes
+		/// </summary>
+		/// <param name="dependencyObject">
+		/// the <see cref="DependencyObject"/> where the property changed
+		/// </param>
+		/// <param name="dependencyPropertyChangedEventArgs">
+		/// some change-related data
+		/// </param>
+		private static void OnStepPropertyChanged (DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+		{
+			UpDownControl upDownControl=(UpDownControl) dependencyObject;
+
+			upDownControl.OnStepChanged ();
+		}
+
+		/// <summary>
+		/// raises the <see cref="StepChangedEvent"/> event
+		/// </summary>
+		protected void OnStepChanged ()
+		{
+			RoutedEventArgs routedEventArgs=new RoutedEventArgs (StepChangedEvent);
+
+			RaiseEvent (routedEventArgs);
+		}
+
+		/// <summary>
 		/// checks <see cref="CurrentValue"/> fulfills some constraints
 		/// </summary>
 		/// <param name="value">
@@ -107,6 +172,28 @@ namespace WPFUpDownControl
 		}
 
 		/// <summary>
+		/// checks <see cref="Step"/> fulfills some constraints
+		/// </summary>
+		/// <param name="value">
+		/// the <see cref="Step"/>
+		/// </param>
+		/// <exception cref="Exception">
+		/// thrown when one of the constraints isn't fulfilled
+		/// </exception>
+		private void CheckStep (decimal? value)
+		{
+			if (value==null)
+			{
+				throw new Exception ("Step must be defined");
+			}
+
+			if (value<=0m)
+			{
+				throw new Exception ("Step value must be more than 0");
+			}
+		}
+
+		/// <summary>
 		/// performs some checks and adjustments when the control is ready to use
 		/// </summary>
 		/// <param name="sender">
@@ -118,6 +205,7 @@ namespace WPFUpDownControl
 		private void UpDownControl_Loaded (object sender, RoutedEventArgs routedEventArgs)
 		{
 			this.CheckCurrentValue (this.CurrentValue);
+			this.CheckStep (this.Step);
 		}
 	}
 }
