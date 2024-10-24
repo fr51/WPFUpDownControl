472b5d4549d1a03bfa7ced1d10acdcb9f21ea9a5
fr51 <francoisemile5@orange.fr>
2024-10-24 21:59:59 +0200

    adding base markup and base code

---
+++ WPFUpDownControl/UpDownControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WPFUpDownControl
+{
+	/// <summary>
+	/// interaction logic for UpDownControl.xaml
+	/// </summary>
+	public partial class UpDownControl : UserControl
+	{
+		/// <summary>
+		/// Constructor. It performs a few value checks before the user uses the control
+		/// </summary>
+		public UpDownControl ()
+		{
+			InitializeComponent ();
+
+			this.Loaded+=this.UpDownControl_Loaded;
+		}
+
+		/// <summary>
+		/// Performs some checks and adjustments when the control is ready to use
+		/// </summary>
+		/// <param name="sender">
+		/// the <see cref="UpDownControl"/> control
+		/// </param>
+		/// <param name="routedEventArgs">
+		/// some event-related data
+		/// </param>
+		private void UpDownControl_Loaded (object sender, RoutedEventArgs routedEventArgs)
+		{
+		}
+	}
+}
